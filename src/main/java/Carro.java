71a16b5f93b636445e1c9bc2aaa892267b0b8fbd
João Pedro Fernandes <75901657+Jp-eng2020@users.noreply.github.com>
2021-07-18 19:33:14 -0300

    Update Carro.java

--- src/main/java/Carro.java
+++ src/main/java/Carro.java
@@ -1,39 +1,105 @@
-public class Carro {
+import jdk.internal.jshell.tool.StopDetectingInputStream;
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
 
+public class Carro {
+   public int passageiros= 0;
+   public int combustivel = 0;
+   public int quilometragem = 0;
     public int getPassageiros() {
-        return -1;
+      
+        return passageiros;
     }
 
     public int getCombustivel() {
-        return -1;
+      
+        return combustivel;
     }
 
     public int getQuilometragem() {
-        return -1;
+        
+        return quilometragem;
     }
 
     public boolean embarcar() {
+       if(passageiros >= 2 ){
         return false;
+
+       }else{
+
+        passageiros += 1;
+        return true;
+    }
+       
     }
 
     public boolean desembarcar() {
-        return false;
+        if(passageiros == 0){
+            return false;
+
+        }else{
+
+            passageiros -= 1;
+            return true;
+        }
+        
     }
 
     public boolean dirigir(int distancia) {
+        
+        if (combustivel == 0) {
+            return false;
+            
+        }
+        if (passageiros >= 0 && combustivel >= 1 ) {
+            
+           
+            if (distancia >= combustivel){
+
+                quilometragem += combustivel;
+                distancia = distancia - combustivel;
+                combustivel = 0;
+                return false;
+            }
+
+            if (distancia<combustivel) {
+                
+                quilometragem += distancia;
+                combustivel = combustivel - distancia;
+                distancia = 0;
+                return true;
+            }
+
+
+        }
+    
+    
+    
         return false;
     }
 
     public boolean abastecer(int quantidade) {
-        return false;
+       
+       if (quantidade <= 0) {
+           return false;
+       }
+       
+       
+        if (quantidade >=100) {
+            combustivel = 100;
+            return true;
+        }else{
+            combustivel += quantidade;
+            return true;
+        }
+       
     }
 
     @Override
     public String toString() {
         return "Carro{" +
-                "passageiros=" + -1 +
-                ", combustivel=" + -1 +
-                ", quilometragem=" + -1 +
+                "passageiros=" + getPassageiros() +
+                ", combustivel=" + getCombustivel() +
+                ", quilometragem=" + getQuilometragem()+
                 '}';
     }
 }
